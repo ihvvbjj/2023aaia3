daf6a49fd6a04fab6612e4003a734e0f8fac92e7
ihvvbjj <12750334@me.mcu.edu.tw>
2024-09-26 16:20:38 +0800

    week03

---
+++ week03/week03-4.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //https://cplusplus.com/reference/string/string/find/
+        return haystack.find(needle);
+
+    }
+};
